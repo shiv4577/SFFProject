0ab34f0f3010fdb1d772b9357204325441cafe43
ASUS <ASUS@192.168.1.101>
2020-05-15 12:18:12 +0530

    commit changes in to SFFBranch1

---
+++ src/test/java/com/w2a/AllTestCase/registraionPageTestCase.java
@@ -0,0 +1,13 @@
+package com.w2a.AllTestCase;
+
+import org.testng.annotations.Test;
+
+public class registraionPageTestCase {
+
+	@Test
+	
+public void registarionPage() {
+		
+		System.out.println("Registraion Page Test Case");
+	}
+}
